1641f2e7d2339f4d48afdd04aa73f0dc1c8431fe
Ben-Moss-Business <benjaminmossbusiness@gmail.com>
2025-11-14 23:31:34 +0000

    Added the placeorder page and am working on checkout

--- src/components/pages/Cart/Cart.jsx
+++ src/components/pages/Cart/Cart.jsx
@@ -1,9 +1,13 @@
 import React, { useContext } from "react";
 import "./cart.css";
 import { StoreContext } from "../../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -44,24 +48,28 @@ const cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>{0}</p>
+              <p>£{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>{2}</p>
+              <p>£{getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>{0}</b>
+              <b>
+                £{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate("/order")}>
+            PROCEED TO CHECKOUT
+          </button>
         </div>
         <div className="cart-promocode">
           <div>
-            <p>If you have a promo cod,e enter it here</p>
+            <p>If you have a promo code enter it here</p>
             <div className="cart-promocode-input">
               <input type="text" placeholder="promo code" />
               <button>Submit</button>
