588cb456991486ca5bd27e93f1d4d0050d99db8f
Ben-Moss-Business <benjaminmossbusiness@gmail.com>
2025-11-19 13:47:45 +0000

    Sign in link to database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express, { response } from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import useRouter from "./routes/userRoute.js";
+import "dotenv/config"
 
 // app config
 const app = express();
@@ -17,6 +19,7 @@ connectDB();
 // api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user",useRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working");
